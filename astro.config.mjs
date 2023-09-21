cdbc8646b56c080668e1e989ccebb9ccfbdfab73
Anthony San Lucas <anthonysanlucas.dev@gmail.com>
2023-09-20 22:19:05 -0500

    Add Astro Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ugdevs.org',
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: page => page !== 'https://ugdevs.org/terminos-condiciones/'
+    })
+  ]
 })
