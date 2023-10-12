c2f20a424f864864465bdd03c0c3af78a94bb7cf
Samuel Rivadeneira <70248758+Samuel19012002@users.noreply.github.com>
2023-10-12 13:53:54 -0500

    Update entrepreneurship.js
    rutas hecho

--- src/data/entrepreneurship.js
+++ src/data/entrepreneurship.js
@@ -22,5 +22,11 @@ export const ENTREPRENEURSHIP = [
     categorys: ['Salud', 'Cuidado Facial', 'Tratamientos'],
     image_path: '/entrepreneurship/martoskincare.webp',
     url: '/emprendimientos/martoskincare'
+  },
+  {
+    name: 'CHIQUIS DONIS',
+    categorys: ['Repostería', 'Pastelería'],
+    image_path: '/entrepreneurship/chiquisdonis.webp',
+    url: '/emprendimientos/chiquisdonis'
   }
 ]
