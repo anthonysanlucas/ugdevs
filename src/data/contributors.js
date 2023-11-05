0e58a7141aa0b8266c3f097b53819740df51e33c
Anthony San Lucas <anthonysanlucas.dev@gmail.com>
2023-11-05 00:44:05 -0500

    Add contributors on the nosotros page

---
+++ src/data/contributors.js
@@ -0,0 +1,22 @@
+export const CONTRIBUTORS_LIST = [
+  {
+    name: 'Edwin Zavala',
+    avatar: '/contributors/edwin-zavala.webp',
+    url: 'https://www.instagram.com/protech_ec/'
+  },
+  {
+    name: 'George Villao',
+    avatar: '/contributors/george-villao.webp',
+    url: 'https://www.tiktok.com/@geovilbal95?_t=8ffiJJrINQz&_r=1'
+  },
+  {
+    name: 'Edison Rodríguez',
+    avatar: '/contributors/edison-rodriguez.webp',
+    url: 'https://instagram.com/edisonrodri?igshid=MzMyNGUyNmU2YQ=='
+  },
+  {
+    name: 'Samuel Rivadeneira',
+    avatar: '/contributors/samuel-rivadeneira.webp',
+    url: 'https://www.linkedin.com/in/samuel-rivadeneira-3478341a7/'
+  }
+]
