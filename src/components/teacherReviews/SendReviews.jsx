8832b29bb7b390dec6a086dcfef33377b0cf2d75
Anthony San Lucas <anthonysanlucas.dev@gmail.com>
2024-01-02 10:38:49 -0500

    Change the design of the teacher page
    - Add SendReviewModal component
    - Update View Reviews layout
    - Update Send Review function

--- src/components/teacherReviews/SendReviews.jsx
+++ src/components/teacherReviews/SendReviews.jsx
@@ -6,6 +6,7 @@ const SendReviews = () => {
   const [review, setReview] = useState('')
   const [isActiveSuccess, setIsActiveSuccess] = useState(false)
   const [error, setError] = useState(null)
+  const [isLoaderActive, setIsLoaderActive] = useState(false)
 
   useEffect(() => {
     const loadTeacherNames = async () => {
@@ -28,7 +29,7 @@ const SendReviews = () => {
 
   const sendReview = async event => {
     event.preventDefault()
-
+    setIsLoaderActive(true)
     const API =
       'https://script.google.com/macros/s/AKfycbx-WwCCee2YR2cGqcy2tv_ZnV6eFOZMWGVycm6lWSBezc-3zqMvVzIHb6MTords8kHw/exec'
 
@@ -52,13 +53,12 @@ const SendReviews = () => {
     setTeacherName('')
     setReview('')
 
-    setTimeout(() => {
-      setIsActiveSuccess(true)
-    }, 150)
+    setIsLoaderActive(false)
+    setIsActiveSuccess(true)
   }
 
   return (
-    <form className="relative w-full max-w-3xl">
+    <form className="relative w-full max-w-3xl" onSubmit={sendReview}>
       <div
         className={`${
           isActiveSuccess ? 'block' : 'hidden'
@@ -111,6 +111,7 @@ const SendReviews = () => {
           value={teacherName}
           onChange={e => setTeacherName(e.target.value)}
           placeholder="Elige un profesor..."
+          required
         />
       </div>
       <datalist id="teacher-list">
@@ -126,14 +127,34 @@ const SendReviews = () => {
         cols="30"
         rows="10"
         maxLength="1000"
-        placeholder="Escribe tu reseña, tus compañeros te lo agradecerán."
+        placeholder={`🚫 Evita lenguaje ofensivo.
+🛠️ Sé constructivo y útil en tus críticas.
+📚 Ofrece estrategias o consejos útiles.
+🗒️ Sé breve y claro.`}
+        required
       />
       <button
-        className="inline-block w-full px-10 py-3 bg-primary text-white font-medium rounded-md shadow-sm shadow-primary-dark/40 transition-colors duration-100 hover:bg-primary-dark hover:shadow-md hover:shadow-primary-dark/40"
+        className="flex justify-center items-center w-full px-10 py-3 bg-primary text-white font-medium rounded-md shadow-sm shadow-primary-dark/40 transition-colors duration-100 hover:bg-primary-dark hover:shadow-md hover:shadow-primary-dark/40 disabled:bg-slate-400"
         type="submit"
-        onClick={sendReview}
-        disabled={!teacherOptions.some(t => t.name === teacherName) || review === ''}>
-        Enviar Reseña
+        disabled={!teacherOptions.some(t => t.name === teacherName) || review === ''}
+        onClick={sendReview}>
+        {isLoaderActive ? (
+          <svg class="animate-spin h-5 w-5 text-white" fill="none" viewBox="0 0 24 24">
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        ) : (
+          'Enviar Reseña'
+        )}
       </button>
     </form>
   )
--- src/components/teacherReviews/ViewReviews.jsx
+++ src/components/teacherReviews/ViewReviews.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
+import SendReviewsModal from '@/components/teacherReviews/SendReviewModal'
 
 const ViewReviews = () => {
   const [reviews, setReviews] = useState([])
   const [search, setSearch] = useState('')
   const [error, setError] = useState(null)
+  const [isOpen, setIsOpen] = useState(false)
 
   useEffect(() => {
     const loadReviews = async () => {
@@ -18,6 +20,8 @@ const ViewReviews = () => {
     }
     loadReviews()
   }, [])
+
+  const openReviewModal = () => setIsOpen(true)
 
   if (error) {
     console.log(error)
@@ -123,7 +127,9 @@ const ViewReviews = () => {
             <p className="mt-2 text-black-medium">No hay ningún comentario</p>
           ) : (
             professor.reviews.map(review => (
-              <div className="my-8 max-w-2xl border border-slate-200 rounded-md px-6 py-4 transition-colors duration-100 hover:bg-primary/20">
+              <div
+                key={crypto.randomUUID()}
+                className="my-8 max-w-2xl border border-slate-200 rounded-md px-6 py-4 transition-colors duration-100 hover:bg-primary/20">
                 <p className="mb-2 text-black-medium">{review.text}</p>
                 <small className="text-black-light text-sm font-medium">{review.date}</small>
               </div>
@@ -135,7 +141,31 @@ const ViewReviews = () => {
   }
 
   return (
-    <section className="w-full max-w-3xl">
+    <section className="w-full">
+      <header className="flex justify-between items-center gap-x-2 mb-10">
+        <SendReviewsModal isOpen={isOpen} setIsOpen={setIsOpen} />
+
+        <h1 className="text-left font-extrabold text-black-dark text-xl sm:text-3xl">
+          Reseñas de Profesores
+        </h1>
+        <button
+          onClick={openReviewModal}
+          className="flex items-center gap-x-4 w-max whitespace-nowrap px-3 py-3 bg-primary text-white font-medium rounded-md text-xs shadow-sm shadow-primary-dark/40 transition-colors duration-100 sm:text-base sm:px-6 hover:bg-primary-dark hover:shadow-md hover:shadow-primary-dark/40">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M11 16.5C11 16.7761 11.2239 17 11.5 17H12.5C12.7761 17 13 16.7761 13 16.5V13.5C13 13.2239 13.2239 13 13.5 13H16.5C16.7761 13 17 12.7761 17 12.5V11.5C17 11.2239 16.7761 11 16.5 11H13.5C13.2239 11 13 10.7761 13 10.5V7.5C13 7.22386 12.7761 7 12.5 7H11.5C11.2239 7 11 7.22386 11 7.5V10.5C11 10.7761 10.7761 11 10.5 11H7.5C7.22386 11 7 11.2239 7 11.5V12.5C7 12.7761 7.22386 13 7.5 13H10.5C10.7761 13 11 13.2239 11 13.5V16.5ZM12 22C10.6167 22 9.31667 21.7375 8.1 21.2125C6.88333 20.6875 5.825 19.975 4.925 19.075C4.025 18.175 3.3125 17.1167 2.7875 15.9C2.2625 14.6833 2 13.3833 2 12C2 10.6167 2.2625 9.31667 2.7875 8.1C3.3125 6.88333 4.025 5.825 4.925 4.925C5.825 4.025 6.88333 3.3125 8.1 2.7875C9.31667 2.2625 10.6167 2 12 2C13.3833 2 14.6833 2.2625 15.9 2.7875C17.1167 3.3125 18.175 4.025 19.075 4.925C19.975 5.825 20.6875 6.88333 21.2125 8.1C21.7375 9.31667 22 10.6167 22 12C22 13.3833 21.7375 14.6833 21.2125 15.9C20.6875 17.1167 19.975 18.175 19.075 19.075C18.175 19.975 17.1167 20.6875 15.9 21.2125C14.6833 21.7375 13.3833 22 12 22ZM12 20C14.2333 20 16.125 19.225 17.675 17.675C19.225 16.125 20 14.2333 20 12C20 9.76667 19.225 7.875 17.675 6.325C16.125 4.775 14.2333 4 12 4C9.76667 4 7.875 4.775 6.325 6.325C4.775 7.875 4 9.76667 4 12C4 14.2333 4.775 16.125 6.325 17.675C7.875 19.225 9.76667 20 12 20Z"
+              fill="white"
+            />
+          </svg>
+          Crear Reseña
+        </button>
+      </header>
+
       <search role="search">
         <label htmlFor="review-search" className="sr-only">
           Buscar reseñas
